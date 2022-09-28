070afb752afd075c24ae535bc0283aa9d1e1c485
Aneesh <katta.aneesh597@gmail.com>
2022-09-28 23:52:53 +0530

    added assignments

---
+++ EmpWageCheckSol/EmpWageCheck/Employeecheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCheck
+{
+    internal class Employeecheck
+    {
+         public static void Employeeattendence()
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == 0)
+                {
+                    Console.WriteLine("Employee is prsent");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                }
+            }
+        }
+    }
+
---
+++ EmpWageCheckSol/EmpWageCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿using EmpWageCheck;
+using System;
+using System.Transactions;
+
+namespace CalEmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Employeecheck.Employeeattendence();
+                    break;
+                case 2:
+                    Employeewage.Dailywage();
+                    break;
+                default:
+                    Console.WriteLine("Entered wrong number");
+                    break;
+
+            }
+
+        }
+    }
+}
