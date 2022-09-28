4ef84c1b6419ccfdd2a7ad4bea5cee54fa0eb1dc
Aneesh <katta.aneesh597@gmail.com>
2022-09-28 23:57:52 +0530

    added day3 employeecheck uc 01

---
+++ employeeattendence/employeecheckin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeattendence
+{
+    internal class employeecheckin
+    {
+            public static void Main()
+            {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == 0)
+                {
+                    Console.WriteLine("Employee is prsent");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                }
+            }
+        }
+    }
+
+
