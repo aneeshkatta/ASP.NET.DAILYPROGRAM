8d29159abacde92065c6bcc991114316b60ab878
Aneesh <katta.aneesh597@gmail.com>
2022-09-30 16:32:48 +0530

    added uc3 employee part time

---
+++ employeefulltimeparttime/employeeparttime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeefulltimeparttime
+{
+    public class employeeparttime
+    {
+        public static void Dailywageparttime()
+        {
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is fulltime");
+                empHrs = 8;
+            }
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is parttime");
+                empHrs = 8;
+            }
+            if (empCheck == 2)
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage is " + empWage);
+        }
+    }
+}
