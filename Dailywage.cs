b8536b364ce31ea1921207eb9d8154abb50a7d91
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-01 21:37:37 +0530

    Add files via upload

---
+++ Dailywage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeswitchcase
+{
+    public class Dailywage
+    {
+        public static void Dailywage1()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            int empHrs = 0;
+                const int EMP_RATE_PER_HOUR = 20;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == 0)
+                {
+                    Console.WriteLine("Employee wage is fulltime");
+                    empHrs = 8;
+                }
+                else
+                {
+                    Console.WriteLine("Employee wage is parttime");
+                    empHrs = 4;
+                }
+                int empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Employee wage is " + empWage);
+            
+        }
+    }
+}
+
---
+++ Empswitchcase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeswitchcase
+{
+    public class Empswitchcase
+    {
+        public static void Main1()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
+
+
