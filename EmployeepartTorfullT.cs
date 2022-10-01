0275cc3f71cb787eae6bce5a439a388b1984ba24
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-01 21:40:43 +0530

    Add files via upload
    added uc5 employee wage problem

---
+++ EmployeepartTorfullT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeswitchcase
+{
+    public class EmployeepartTorfullT
+    {
+        public static void Empcheck()
+        {
+                Console.WriteLine("Welcome to Employee Wage Computation Program ");
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == 0)
+                {
+                    Console.WriteLine("Employee is fulltime");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is parttime");
+                }
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeswitchcase
+
+{
+    public class program
+    {
+        private static readonly int empCheck;
+
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("enter a) A value one to check employee attendence b) A value two to check employee part time or full time worker c) A value three to check employee salary" + EmpCheck);
+            //Random random = new Random();
+            //int EmpCheck = random.Next(0, 3); 
+            //int EmpCheck;
+
+            Console.WriteLine("Enter Number\n 1.To check Employe Present/absent\t 2.To check Employee is partTime or fullTime\t 3. To check Employee daily wage");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Empswitchcase.Main1();
+                    break;
+
+                case 2:
+                    EmployeepartTorfullT.Empcheck();
+                    break;
+                case 3:
+                    Dailywage.Dailywage1();
+                    break;
+                default:
+                    break;
+
+            }
+         }
+
+    }
+ }
